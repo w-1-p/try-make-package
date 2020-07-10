7de5ec60044689e294509a9c0e604589d5367dd9
王奕平 <wangyipingyouxiang@163.com>
2020-07-10 18:14:07 +0800

    init && dev package

---
+++ src/EchoHello.php
@@ -0,0 +1,13 @@
+<?php
+// +----------------------------------------------------------------------
+// | Author: 王奕平 wyp <407213504@qq.com>. Date:2020/7/10 Time:17:52
+// +----------------------------------------------------------------------
+namespace Wangyipinglove\TryMakePackage;
+
+class EchoHello
+{
+    public static function say()
+    {
+        echo "hello";
+    }
+}
